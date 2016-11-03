3d967422ea2e7299cd6bdacdd733d012b330527f
Thai Dang <thaidang@uark.edu>
2016-11-03 13:29:07 -0500

    add a create product screen

---
+++ Inventory-management/Create Product Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory_management
+{
+    public partial class Create_Product_Form : Form
+    {
+        public Create_Product_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
