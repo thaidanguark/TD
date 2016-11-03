bf39043f51adb9dfb1e35add1a4521aecfb9401d
Thai Dang <thaidang@uark.edu>
2016-11-03 13:22:29 -0500

    add issue good receipt screen

---
+++ Inventory-management/Issuegoodsform.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Inventory_management
+{
+    partial class Issuegoodsform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.Submit = new System.Windows.Forms.Button();
+            this.ProductID = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(22, 70);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            // 
+            // Submit
+            // 
+            this.Submit.Location = new System.Drawing.Point(12, 195);
+            this.Submit.Name = "Submit";
+            this.Submit.Size = new System.Drawing.Size(75, 23);
+            this.Submit.TabIndex = 1;
+            this.Submit.Text = "button1";
+            this.Submit.UseVisualStyleBackColor = true;
+            // 
+            // ProductID
+            // 
+            this.ProductID.AutoSize = true;
+            this.ProductID.Location = new System.Drawing.Point(43, 9);
+            this.ProductID.Name = "ProductID";
+            this.ProductID.Size = new System.Drawing.Size(35, 13);
+            this.ProductID.TabIndex = 2;
+            this.ProductID.Text = "label1";
+            this.ProductID.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Issuegoodsform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.ProductID);
+            this.Controls.Add(this.Submit);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Issuegoodsform";
+            this.Text = "Issuegoodsform";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button Submit;
+        private System.Windows.Forms.Label ProductID;
+    }
+}
---
+++ Inventory-management/Issuegoodsform.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory_management
+{
+    public partial class Issuegoodsform : Form
+    {
+        public Issuegoodsform()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
